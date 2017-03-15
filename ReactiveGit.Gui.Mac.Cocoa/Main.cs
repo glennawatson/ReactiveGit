4610ff8cb40be903ce6dc02df9b747d2fb2ac4c7
Glenn Watson <glenn@glennwatson.net>
2017-03-16 10:00:13 +1100

    Adding Mac Cocoa project for ReactiveGIT.

---
+++ ReactiveGit.Gui.Mac.Cocoa/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace ReactiveGit.Gui.Mac.Cocoa
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.Main(args);
+		}
+	}
+}
